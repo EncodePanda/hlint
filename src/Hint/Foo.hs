5314af9cb6564a1285956302670e72046a719b2b
Pawel Szulc <pawel.szulc@klarna.com>
2020-05-08 13:27:26 +0200

    Works but why? ;)

--- src/Hint/Foo.hs
+++ src/Hint/Foo.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE ViewPatterns #-}
 module Hint.Foo where
 
 import Hint.Type
@@ -6,6 +7,8 @@ import GHC.Hs.Decls
 import GHC.Hs
 import SrcLoc
 import Language.Haskell.GhclibParserEx.GHC.Utils.Outputable
+import Data.Generics.Uniplate.Operations
+import OccName
 
 data WarnFoo = WarnFoo
     { newDecl :: LHsDecl GhcPs
@@ -14,7 +17,7 @@ data WarnFoo = WarnFoo
 fooHint :: DeclHint'
 fooHint _ _ old
   | Just WarnFoo{newDecl} <- fooField old
-  = [(suggestN' "Consider not using data Foo" old newDecl)]
+  = [(suggestN' "Consider not using data Foo, maybe FooBar?" old newDecl)]
 fooHint _ _ _ = []
 
 -- data Foo = ...
@@ -22,9 +25,15 @@ fooField :: LHsDecl GhcPs -> Maybe WarnFoo
 fooField (L loc (TyClD ext decl@(DataDecl _ name  _ _ _)))
     | typeNameIsFoo name =
         Just WarnFoo
-              { newDecl = L loc $ TyClD ext decl
+              { newDecl = L loc $ TyClD ext decl {tcdLName = toTcdLName "Bar" name}
               }
 fooField _ = Nothing
 
 
 typeNameIsFoo name = unsafePrettyPrint name == "Foo"
+
+-- toTcdLName :: String -> Located (IdP GhcPs)
+toTcdLName newName originalDecl = transformBi replace originalDecl
+  where
+    replace :: OccName -> OccName
+    replace (unsafePrettyPrint -> name) = mkOccName srcDataName "Bar"
